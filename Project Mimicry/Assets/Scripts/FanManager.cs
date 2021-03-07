3a2e7ff88d740970a120f3f0373d228eb56c6777
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-03-07 11:59:37 +0100

    Starting Fan mechanic

---
+++ Project Mimicry/Assets/Scripts/FanManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanManager : MonoBehaviour
+{
+    public bool fanIsON;
+    public GameObject objectToPush;
+
+    [SerializeField] float pushForce;
+    [SerializeField] float lerpSpeed;
+
+    [SerializeField] GameObject fanCollider;
+    private Vector3 triggerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerPos = GetComponentInChildren<FanTrigger>().transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fanIsON && objectToPush != null)
+        {
+            pushObject(objectToPush);
+        }
+        else if(!fanIsON && fanCollider.activeSelf == true)
+        {
+            fanCollider.SetActive(false);
+        }
+    }
+
+    private void pushObject(GameObject cubePush)
+    {
+        if (Vector3.Distance(cubePush.transform.position, transform.forward * pushForce + triggerPos) < 0.02f)
+        {
+            cubePush.transform.position = transform.forward * pushForce + triggerPos;
+            objectToPush = null;
+        }
+        else
+        {
+            cubePush.transform.position = Vector3.Lerp(cubePush.transform.position, transform.forward * pushForce + triggerPos, lerpSpeed * Time.deltaTime);
+        }
+    }
+
+    public void setFanCollider(bool colBool)
+    {
+        fanCollider.SetActive(colBool);
+    }
+}
---
+++ Project Mimicry/Assets/Scripts/FanTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanTrigger : MonoBehaviour
+{
+    FanManager fanManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fanManager = GetComponentInParent<FanManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ObjectObstacle"))
+        {
+            fanManager.objectToPush = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ObjectObstacle"))
+        {
+            if (fanManager.fanIsON)
+            {
+                fanManager.setFanCollider(true);
+            }
+            else
+            {
+                fanManager.objectToPush = null;
+            }
+        }
+    }
+}
