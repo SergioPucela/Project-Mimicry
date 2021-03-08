755e576727a23170159435b2041f949b6bddcb21
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-03-08 17:53:02 +0100

    Lasers working with object pool (testing ways to replicate the lasers on the cubes)

--- Project Mimicry/Assets/Scripts/LaserCube.cs
+++ Project Mimicry/Assets/Scripts/LaserCube.cs
@@ -5,40 +5,107 @@ using UnityEngine;
 public class LaserCube : MonoBehaviour
 {
     public bool isCubeIgnited;
+
+    [SerializeField] float updateFrequency;
     [SerializeField] LineRenderer LaserPrefab;
     [SerializeField] float laserRange;
+    [SerializeField] LayerMask layerMask;
+
+    private float timer = 0f;
 
     public List<GameObject> LaserOrigins = new List<GameObject>();
 
-    //Debug
-    bool isTrigger;
+    private Queue<GameObject> lasers = new Queue<GameObject>();
+
+    private ObjectPooler objectPooler;
+    private GameObject currentLaser;
+    private LineRenderer mLineRenderer;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        objectPooler = ObjectPooler.Instance;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (isCubeIgnited && !isTrigger)
+        if (isCubeIgnited)
         {
-            generateAllLasers();
-            print("LASERS EVERYWHERE");
-
-            //Debug
-            isTrigger = true;
+            if(timer >= updateFrequency)
+            {
+                timer = 0f;
+                while(lasers.Count != 0)
+                {
+                    lasers.Dequeue().SetActive(false);
+                }
+                generateAllLasers();
+            }
+            timer += Time.fixedDeltaTime;
+        }
+        else if(lasers.Count > 0)
+        {
+            while (lasers.Count != 0)
+            {
+                lasers.Dequeue().SetActive(false);
+            }
         }
     }
 
     private void generateAllLasers()
     {
-        foreach(GameObject laserDisplay in LaserOrigins) //TO DO: MAKE THIS WORK WITH OBJECT POOL
+        foreach(GameObject laserDisplay in LaserOrigins)
         {
+            RaycastHit hit;
+            if (Physics.Raycast(laserDisplay.transform.position, laserDisplay.transform.forward, out hit, laserRange, layerMask))
+            {
+                createNewLaser(laserDisplay.transform.position, hit.point, laserDisplay.transform.rotation);
+
+                if (hit.transform.gameObject.CompareTag("DisplayLaser"))
+                {
+                    LaserDisplay reflectLaser = hit.transform.gameObject.GetComponent<LaserDisplay>();
+                    reflectLaser.isReflecting = true;
+                }
+            }
+
+            /*
             LineRenderer laser = Instantiate(LaserPrefab);
             laser.SetPosition(0, laserDisplay.transform.position);
-            laser.SetPosition(1, laserDisplay.transform.forward * laserRange + laserDisplay.transform.position);
+            laser.SetPosition(1, laserDisplay.transform.forward * laserRange + laserDisplay.transform.position); //Esta línea me puede servir para debug
+            */
         }
+    }
+
+    /*
+    public void reflectLasers(GameObject laserToAvoid)
+    {
+        foreach (GameObject laserDisplay in LaserOrigins)
+        {
+            if(laserDisplay != laserToAvoid)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(laserDisplay.transform.position, laserDisplay.transform.forward, out hit, laserRange, layerMask))
+                {
+                    createNewLaser(laserDisplay.transform.position, hit.point, laserDisplay.transform.rotation);
+
+                    if (hit.transform.gameObject.CompareTag("DisplayLaser"))
+                    {
+                        LaserDisplay reflectLaser = hit.transform.gameObject.GetComponent<LaserDisplay>();
+                        reflectLaser.isReflecting = true;
+                    }
+                }
+            }
+        }
+    }*/
+
+    private void createNewLaser(Vector3 initPos, Vector3 endPos, Quaternion rotation)
+    {
+        currentLaser = objectPooler.SpawnFromPool("Laser", initPos, rotation);
+        mLineRenderer = currentLaser.GetComponent<LineRenderer>();
+
+        lasers.Enqueue(currentLaser);
+
+        mLineRenderer.SetPosition(0, initPos);
+        mLineRenderer.SetPosition(1, endPos);
     }
 }
---
+++ Project Mimicry/Assets/Scripts/LaserDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDisplay : MonoBehaviour
+{
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] float viewRange;
+    public bool isReflecting;
+
+    private LaserCube parentCube;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        parentCube = GetComponentInParent<LaserCube>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        checkReflect();
+        if (isReflecting)
+        {
+            parentCube.reflectLasers(this.gameObject);
+        }
+        */
+    }
+
+    private void checkReflect()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, viewRange, layerMask) && isReflecting)
+        {
+            if (!hit.transform.gameObject.CompareTag("DisplayLaser"))
+            {
+                isReflecting = false;
+                print("OHAYOO");
+            }
+        }
+    }
+}
