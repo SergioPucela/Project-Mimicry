1ff91bcb19fa1528409c078a3da24b76508c23d9
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-03-14 15:56:41 +0100

    QOL changes

--- Project Mimicry/Assets/Scripts/VcamTrigger.cs
+++ Project Mimicry/Assets/Scripts/VcamTrigger.cs
@@ -9,8 +9,24 @@ public class VcamTrigger : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera vcamPrior;
     [SerializeField] List<CinemachineVirtualCamera> vcamsNonPrior = new List<CinemachineVirtualCamera>();
 
+    private bool isButtonDown;
+
+    private void Update()
+    {
+        if (isButtonDown)
+        {
+            if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+            {
+                isButtonDown = false;
+                player.calculateInputFromCamera();
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        isButtonDown = true;
+
         if (other.CompareTag("Player"))
         {
             vcamPrior.enabled = true;
@@ -19,14 +35,6 @@ public class VcamTrigger : MonoBehaviour
             {
                 vcam.enabled = false;
             }
-
-            StartCoroutine("calculateInputValues");
         }
-    }
-
-    private IEnumerator calculateInputValues()
-    {
-        yield return new WaitForSeconds(0.9f);
-        player.calculateInputFromCamera();
     }
 }
