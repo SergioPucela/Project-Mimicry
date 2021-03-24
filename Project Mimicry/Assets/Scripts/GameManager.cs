5a4eda9a11c8432c0e436d804dff9e151862dec1
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-03-24 15:58:33 +0100

    Transition between levels (just for testing a build) and reload puzzle option added

--- Project Mimicry/Assets/Scripts/GameManager.cs
+++ Project Mimicry/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,9 +19,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.R))
+        {
+            print("Reload level");
+            reloadLevel();
+        }
         if (checkWinCons())
         {
             print("YOU WIN!");
+            StartCoroutine("loadNextLevel");
         }
     }
 
@@ -46,5 +53,23 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(0.25f);
 
         playerControl.enabled = true;
+    }
+
+    private IEnumerator loadNextLevel()
+    {
+        yield return new WaitForSeconds(1f);
+        if (SceneManager.GetActiveScene().buildIndex == 8)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private void reloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
