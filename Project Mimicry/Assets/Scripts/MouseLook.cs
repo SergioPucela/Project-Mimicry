8ae37d9c1d5980407ac2074d099cc247d70e2f5f
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-03-31 18:02:33 +0200

    Starting the second FP scene

---
+++ Project Mimicry/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private float mouseX;
+    private float mouseY;
+
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
+    [SerializeField] float xLockAngle = 60f;
+    [SerializeField] float yLockAngle = 60f;
+
+    [SerializeField] float mouseSensitivity = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -xLockAngle, xLockAngle);
+        yRotation = Mathf.Clamp(yRotation, -yLockAngle, yLockAngle);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
