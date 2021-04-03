b35247d7ecf2e54e22555469158abfe92f0bc5e0
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-04-03 18:43:21 +0200

    Exam Scene working
    Needs to be improved visually, but it's overall working as expected.

--- Project Mimicry/Assets/Scripts/ExamManager.cs
+++ Project Mimicry/Assets/Scripts/ExamManager.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class ExamManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> arrows = new List<GameObject>();
-    [SerializeField] private List<GameObject> checkBox = new List<GameObject>();
+    [SerializeField] private List<Renderer> checkBox = new List<Renderer>();
+
+    [Header("Materials")]
+    [SerializeField] Material incorrectMat;
+    [SerializeField] Material correctMat;
+    [SerializeField] Material neutralMat;
+    [SerializeField] Material examMaterial;
+
+    [Header("Exam")]
+    [SerializeField] Renderer exam;
+
+    [Header("Light")]
+    [SerializeField] GameObject projectorLight;
 
     private int lastRandIndex = -1;
+    private int currentQuestion = 0;
+
+    private bool examRunning = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,37 +33,35 @@ public class ExamManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if (examRunning)
         {
-            if (lastRandIndex == 1)
-                print("Correcto");
-            else
-                print("Incorrecto");
-            nextQuestion(generateRandIndex());
-        }
-        else if(Input.GetKeyDown(KeyCode.A))
-        {
-            if (lastRandIndex == 2)
-                print("Correcto");
-            else
-                print("Incorrecto");
-            nextQuestion(generateRandIndex());
-        }
-        else if(Input.GetKeyDown(KeyCode.S))
-        {
-            if (lastRandIndex == 0)
-                print("Correcto");
-            else
-                print("Incorrecto");
-            nextQuestion(generateRandIndex());
-        }
-        else if(Input.GetKeyDown(KeyCode.D))
-        {
-            if (lastRandIndex == 3)
-                print("Correcto");
-            else
-                print("Incorrecto");
-            nextQuestion(generateRandIndex());
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                if (lastRandIndex == 1)
+                    checkAnswer(true);
+                else
+                    checkAnswer(false);            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (lastRandIndex == 2)
+                    checkAnswer(true);
+                else
+                    checkAnswer(false);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                if (lastRandIndex == 0)
+                    checkAnswer(true);
+                else
+                    checkAnswer(false);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (lastRandIndex == 3)
+                    checkAnswer(true);
+                else
+                    checkAnswer(false);
+            }
         }
     }
 
@@ -73,5 +86,49 @@ public class ExamManager : MonoBehaviour
         int numRand = Random.Range(0, 4);
 
         return numRand;
+    }
+
+    private void checkAnswer(bool isCorrect)
+    {
+        if (isCorrect)
+            checkBox[currentQuestion].material = correctMat;
+        else
+            checkBox[currentQuestion].material = incorrectMat;
+
+        currentQuestion++;
+        nextQuestion(generateRandIndex());
+
+        if (currentQuestion >= checkBox.Count)
+        {
+            examRunning = false;
+            StartCoroutine("resetExam");
+        }
+    }
+
+    private IEnumerator resetExam()
+    {
+        foreach (GameObject GO in arrows)
+        {
+            if (!GO.activeSelf) GO.SetActive(true);
+        }
+        currentQuestion = 0;
+
+        yield return new WaitForSeconds(1f);
+
+        foreach(Renderer CB in checkBox)
+        {
+            CB.material = neutralMat;
+        }
+        exam.material = neutralMat;
+        lastRandIndex = -1;
+        projectorLight.SetActive(false);
+
+        yield return new WaitForSeconds(1f);
+
+        exam.material = examMaterial;
+        examRunning = true;
+        projectorLight.SetActive(true);
+
+        nextQuestion(generateRandIndex());
     }
 }
