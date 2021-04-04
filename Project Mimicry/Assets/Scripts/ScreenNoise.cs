83bf49ea0ce6661ebd1c4434888121d473ca145d
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-04-04 19:50:27 +0200

    Screen noise and screen spawner added

---
+++ Project Mimicry/Assets/Scripts/ScreenNoise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenNoise : MonoBehaviour
+{
+    private Renderer screen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screen = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screen.sharedMaterial.SetFloat("_Factor1", Random.Range(1000, 3000));
+        screen.sharedMaterial.SetFloat("_Factor2", Random.Range(400, 800));
+        screen.sharedMaterial.SetFloat("_Factor3", Random.Range(4000, 6000));
+    }
+}
---
+++ Project Mimicry/Assets/Scripts/ScreenSpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSpawnManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> screens;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach(GameObject GO in screens)
+            {
+                StartCoroutine(spawnScreen(GO));
+            }
+        }
+    }
+
+    private IEnumerator spawnScreen(GameObject screen)
+    {
+        yield return new WaitForSeconds(Random.Range(0.15f, 0.75f));
+        screen.SetActive(true);
+    }
+}
