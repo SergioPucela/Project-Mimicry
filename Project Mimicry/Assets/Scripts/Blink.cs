111329bd809b171bbe8f62dadc3ab7a07a142e38
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-04-07 18:02:00 +0200

    Third FP scene finished!

---
+++ Project Mimicry/Assets/Scripts/Blink.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    [SerializeField] GameObject blackPanel;
+    [SerializeField] GameObject characterDummy;
+    [SerializeField] float blinkTime;
+
+    private bool alreadyBlinked;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !alreadyBlinked)
+        {
+            StartCoroutine("blink");
+        }
+    }
+
+    private IEnumerator blink()
+    {
+        alreadyBlinked = true;
+
+        yield return new WaitForSeconds(blinkTime);
+        blackPanel.SetActive(true);
+        characterDummy.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        blackPanel.SetActive(false);    
+    }
+}
--- Project Mimicry/Assets/Scripts/ScreenSpawnManager.cs
+++ Project Mimicry/Assets/Scripts/ScreenSpawnManager.cs
@@ -7,6 +7,12 @@ public class ScreenSpawnManager : MonoBehaviour
     [SerializeField] List<GameObject> screens = new List<GameObject>();
     [SerializeField] List<GameObject> screensToDisable = new List<GameObject>();
 
+    [Header("Control the delay spawn")]
+    [SerializeField] bool isDelayed;
+    [SerializeField] float displayDelay;
+
+    private bool alreadyTriggered;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -16,7 +22,7 @@ public class ScreenSpawnManager : MonoBehaviour
                 StartCoroutine(spawnScreen(GO));
             }
 
-            if(screensToDisable.Count > 0)
+            if(screensToDisable.Count > 0 && !isDelayed)
             {
                 foreach (GameObject GO in screensToDisable)
                 {
@@ -28,7 +34,22 @@ public class ScreenSpawnManager : MonoBehaviour
 
     private IEnumerator spawnScreen(GameObject screen)
     {
-        yield return new WaitForSeconds(Random.Range(0.15f, 0.75f));
-        screen.SetActive(true);
+        if(isDelayed)
+        {
+            yield return new WaitForSeconds(displayDelay);
+            if (screensToDisable.Count > 0)
+            {
+                foreach (GameObject GO in screensToDisable)
+                {
+                    GO.SetActive(false);
+                }
+            }
+            screen.SetActive(true);
+        }
+        else
+        {
+            yield return new WaitForSeconds(Random.Range(0.15f, 0.75f));
+            screen.SetActive(true);
+        }
     }
 }
