0634db7d23231066522f195ca6b2809a9e145339
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-05-01 19:05:51 +0200

    Volume slider added

---
+++ Project Mimicry/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    private static float currentSliderValue = 0.7f;
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = currentSliderValue;
+    }
+
+    public void SetVolumeLevel(float sliderValue)
+    {
+        mixer.SetFloat("AudioVolume", Mathf.Log10(sliderValue) * 20);
+
+        currentSliderValue = sliderValue;
+    }
+}
